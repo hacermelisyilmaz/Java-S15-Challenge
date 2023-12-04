d671317d4f6127f347933e9dfd8faf0566e9ba64
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-04 21:47:35 +0300

    Student class with borrowedBooks list created

---
+++ src/com/workintech/models/person/Student.java
@@ -0,0 +1,19 @@
+package com.workintech.models.person;
+
+import com.workintech.enums.PersonType;
+import com.workintech.models.book.Book;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Student extends Member {
+    private Map<String, Book> borrowedBooks = new HashMap<>();
+
+    public Student(String name, PersonType type, long memberID, String dateOfMembership) {
+        super(name, type, memberID, dateOfMembership);
+    }
+
+    public Map<String, Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+}
