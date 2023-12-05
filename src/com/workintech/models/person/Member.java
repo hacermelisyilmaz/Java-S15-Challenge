dea83af2bea7a8a4ab63c349ab7a42341fae02e8
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-05 22:08:14 +0300

    borrowBook method is completed

--- src/com/workintech/models/person/Member.java
+++ src/com/workintech/models/person/Member.java
@@ -1,10 +1,16 @@
 package com.workintech.models.person;
 
+import com.workintech.enums.BookStatus;
 import com.workintech.enums.PersonType;
+import com.workintech.models.Library;
+import com.workintech.models.book.Book;
 
-public abstract class Member extends Person{
+import java.util.*;
+
+public abstract class Member extends Person {
     private long memberID;
     private String dateOfMembership;
+    private Map<Long, Book> borrowedBooks = new TreeMap<>();
     private int numberOfBooksBorrowed;
     private double bill;
 
@@ -12,6 +18,19 @@ public abstract class Member extends Person{
         super(name, type);
         this.memberID = memberID;
         this.dateOfMembership = dateOfMembership;
+    }
+
+    public void borrowBook(Long newISBN) {
+        boolean flag = false;
+
+        if (Library.getBooks().containsKey(newISBN)) flag = true;
+        else System.out.println("Book not found in the system.");
+
+        if (flag && Library.getBook(newISBN).getStatus() == BookStatus.AVAILABLE) {
+            borrowedBooks.put(newISBN, Library.getBook(newISBN));
+            Library.getBook(newISBN).setStatus(BookStatus.LENT);
+        }
+        else System.out.println("The book is not available.");
     }
 
     public long getMemberID() {
