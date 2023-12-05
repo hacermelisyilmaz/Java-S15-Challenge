3c3b7aa7edd50838f5363e4f248a62d0b3350f51
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-05 22:08:46 +0300

    getBook method by passing ISBN

--- src/com/workintech/models/Library.java
+++ src/com/workintech/models/Library.java
@@ -1,12 +1,19 @@
 package com.workintech.models;
 
+import com.workintech.models.book.Book;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Library {
-    private static Map books = new HashMap<>();
+    private static Map<Long, Book> books = new HashMap<>();
 
-    public static Map getBooks() {
+    public static Map<Long, Book> getBooks() {
         return books;
+    }
+
+    public static Book getBook (long ISBN) {
+        return books.get(ISBN);
     }
 }
