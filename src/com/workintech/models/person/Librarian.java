387c5422d561fffd14e4657ae4219a2cfd5d37aa
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-05 17:04:21 +0300

    editBookInfo method created in Librarian class

--- src/com/workintech/models/person/Librarian.java
+++ src/com/workintech/models/person/Librarian.java
@@ -31,7 +31,51 @@ public class Librarian extends Person {
         else System.out.println("Incorrect password.");
     }
 
+    public void editBookInfo(long ISBN) {
+        Scanner scanner = new Scanner(System.in);
+        if (authenticate()) {
+            String type = "";
+            while (type.equals("exit")) {
+                System.out.println("Choose what to change (name, author, edition, year, publisher, price). Type 'exit' to exit: ");
+                type = scanner.nextLine().toLowerCase(Locale.ENGLISH);
 
+                switch (type) {
+                    case "exit":
+                        break;
+                    case "name":
+                        System.out.println("Type the new " + type + ": ");
+                        String newName = scanner.nextLine();
+                        ((Book)Library.getBooks().get(ISBN)).setName(newName);
+                    case "author":
+                        System.out.println("Type the first name of the " + type + ": ");
+                        String first = scanner.nextLine();
+                        System.out.println("Type the last name of the " + type + ": ");
+                        String second = scanner.nextLine();
+                        ((Book)Library.getBooks().get(ISBN)).setAuthor(first, second);
+                    case "edition":
+                        System.out.println("Type the new " + type + ": ");
+                        int newEdition = scanner.nextInt();
+                        ((Book)Library.getBooks().get(ISBN)).setEdition(newEdition);
+                    case "year":
+                        System.out.println("Type the new " + type + ": ");
+                        int newYear = scanner.nextInt();
+                        ((Book)Library.getBooks().get(ISBN)).setYear(newYear);
+                    case "publisher":
+                        System.out.println("Type the new " + type + ": ");
+                        String newPublisher = scanner.nextLine();
+                        ((Book)Library.getBooks().get(ISBN)).setPublisher(newPublisher);
+                    case "price":
+                        System.out.println("Type the new " + type + ": ");
+                        double newPrice = scanner.nextDouble();
+                        ((Book)Library.getBooks().get(ISBN)).setPrice(newPrice);
+                    default:
+                        System.out.println("Incorrect field. Please choose one of the following: name, author, edition, year, publisher, price");
+                }
+
+            }
+        }
+        else System.out.println("Incorrect password.");
+    }
 
     public void createBill() {
 
