57fdb30a91d4f8fddb6ec72ad4b74b0241727630
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-05 17:06:33 +0300

    Books map and its getter

--- src/com/workintech/models/Library.java
+++ src/com/workintech/models/Library.java
@@ -1,4 +1,12 @@
 package com.workintech.models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Library {
+    private static Map books = new HashMap<>();
+
+    public static Map getBooks() {
+        return books;
+    }
 }
