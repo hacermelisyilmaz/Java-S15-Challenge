a71ad5c7080566490828d93ab37f4e0f38b5eac2
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-05 23:27:59 +0300

    members & books Map turned into TreeMap getMember(s) methods are created

--- src/com/workintech/models/Library.java
+++ src/com/workintech/models/Library.java
@@ -1,13 +1,16 @@
 package com.workintech.models;
 
 import com.workintech.models.book.Book;
+import com.workintech.models.person.Member;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class Library {
-    private static Map<Long, Book> books = new HashMap<>();
+    private static Map<Long, Book> books = new TreeMap<>();
+    private static Map<Long, Member> members = new TreeMap<>();
 
     public static Map<Long, Book> getBooks() {
         return books;
@@ -16,4 +19,14 @@ public class Library {
     public static Book getBook (long ISBN) {
         return books.get(ISBN);
     }
+
+    public static Map<Long, Member> getMembers() {
+        return members;
+    }
+
+    public static Member getMember (long ID) {
+        return members.get(ID);
+    }
+
+
 }
