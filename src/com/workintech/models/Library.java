82b36b62185006c30591a7b13dc2d1c0e809d0f1
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-06 14:25:21 +0300

    Setters for the static members and books Maps

--- src/com/workintech/models/Library.java
+++ src/com/workintech/models/Library.java
@@ -3,14 +3,12 @@ package com.workintech.models;
 import com.workintech.models.book.Book;
 import com.workintech.models.person.Member;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+
 
 public class Library {
-    private static Map<Long, Book> books = new TreeMap<>();
-    private static Map<Long, Member> members = new TreeMap<>();
+    private static Map<Long, Book> books;
+    private static Map<Long, Member> members;
 
     public static Map<Long, Book> getBooks() {
         return books;
@@ -28,5 +26,13 @@ public class Library {
         return members.get(ID);
     }
 
+    public static void setBooks(Book newBook) {
+        if (books.containsKey(newBook.getISBN())) System.out.println("This book is already in the system.");
+        else books.put(newBook.getISBN(), newBook);
+    }
 
+    public static void setMembers(Member newMember) {
+        if (members.containsKey(newMember.getMemberID())) System.out.println("A member with the given ID is already in the system.");
+        else members.put(newMember.getMemberID(), newMember);
+    }
 }
