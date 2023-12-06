d89bf24bd2ef1506cc02a86f6bdeb13dc68a468e
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-06 15:48:07 +0300

    New members created

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 import com.workintech.models.Library;
 import com.workintech.models.book.Author;
 import com.workintech.models.book.Book;
-import com.workintech.models.person.Member;
+import com.workintech.models.person.Faculty;
 import com.workintech.models.person.Student;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,6 +21,12 @@ public class Main {
                 new Book(9789753632904L, new Author("Füruzan", "NA", 1932), "Benim Sinemalarım",
                         1973, 2, "NA", "Yapı Kredi Yayınları", 3.90, "2023/02/22"));
 
-
+        /* ADDING MEMBERS */
+        Library.addMember(
+                new Student("Melis Yılmaz", 2023404001, "2023/06/23", new TreeMap<>()),
+                new Student("Miray Tunç", 2022415001, "2022/11/25", new TreeMap<>()),
+                new Faculty("Doğancan  Kınık", 2022400002, "2022/05/02", new TreeMap<>()),
+                new Faculty("Erhan Fırat", 2019400001, "2019/06/03", new TreeMap<>())
+        );
     }
 }
